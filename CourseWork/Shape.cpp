9ac53ddd2e5644e506acdd1ef1aa10a4ce4a8632
Ben Barton-Foskett <bbarfoz@gmail.com>
2024-05-15 15:04:06 +0100

    Added bubble sort to sort vertex 2D array, started on getting rid of unnecesary rendering

--- CourseWork/Shape.cpp
+++ CourseWork/Shape.cpp
@@ -8,7 +8,8 @@
 #include "Shape.h"
 
 void Shape::draw() {
-    for (it1 = sides.begin(); it1 != sides.end(); it1++, it++) {
+    checkSides();
+    for (it1 = sides.begin(); it1 != sides.end(); it1++) {
         glBegin(GL_QUADS);
         for (it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
             (*it2).getProjectedVertex().drawGlVertex();
@@ -20,11 +21,11 @@ void Shape::draw() {
 void Shape::rotate(float ax, float ay) {
 	list<list<Vertex>> temp;
 	for (it1 = sides.begin(); it1 != sides.end(); it1++) {
-		list<Vertex> temp1;
+		list<Vertex> side;
 		for (it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
-			temp1.push_back((*it2).rotate(ax, ay));
+			side.push_back((*it2).rotate(ax, ay));
 		}
-		temp.push_back(temp1);
+		temp.push_back(side);
 	}
 	sides.clear();
 	for (it1 = temp.begin(); it1 != temp.end(); it1++) {
@@ -34,4 +35,65 @@ void Shape::rotate(float ax, float ay) {
 		}
 		sides.push_back(side);
 	}
+}
+
+void Shape::checkSides() {
+    list<list<Vertex>> temp;
+    for (it1 = sides.begin(); it1 != sides.end(); it1++) {
+        list<Vertex> side;
+        for (it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
+            side.push_back((*it2).getProjectedVertex());
+        }
+        temp.push_back(side);
+    }
+    sides.clear();
+    for (it1 = temp.begin(); it1 != temp.end(); it1++) {
+        list<Vertex> side;
+        for (it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
+            side.push_back((*it2));
+        }
+        sides.push_back(side);
+    }
+    temp.clear();
+    temp = sort2DList(sides);
+    for (it2 = sideCentres.begin(); it2 != sideCentres.end(); it2++) {
+        for (it1 = temp.begin(); it1 != temp.end(); it1++) {
+            list<Vertex>::iterator it;
+            for (it = (*it1).begin(); it != (*it1).end(); it++) {
+                //TODO: CHECK IF PROJECTED CENTRE IS IN THE BOUNDS OF ONE OF THE SIDES.
+            }
+        }
+    }
+}
+
+list<list<Vertex>> Shape::sort2DList(list<list<Vertex>> l) {
+    list<list<Vertex>> temp;
+    for (it1 = l.begin(); it1 != l.end(); it1++) {
+        list<Vertex> sorted = sortList((*it1));
+        temp.push_back(sorted);
+    }
+    return temp;
+}
+
+list<Vertex> Shape::sortList(list<Vertex> l, int mode=0) {
+    for (it2 = l.begin(); it2 != l.end()--; it2++) {
+        list<Vertex>::iterator it;
+        switch (mode) {
+        case 0:
+            for (it = l.begin()++; it != l.end()--; it++) {
+                if ((*it).getX() > (*std::next(it)).getX()) {
+                    std::swap(*it, *std::next(it));
+                }
+            }
+            break;
+        case 1:
+            for (it = l.begin()++; it != l.end()--; it++) {
+                if ((*it).getY() > (*std::next(it)).getY()) {
+                    std::swap(*it, *std::next(it));
+                }
+            }
+            break;
+        }
+    }
+    return l;
 }
--- CourseWork/Shape.h
+++ CourseWork/Shape.h
@@ -10,14 +10,23 @@ private:
     list<Vertex> sideCentres;
 	list<list<Vertex>>::iterator it1;
 	list<Vertex>::iterator it2;
+    virtual void checkSides();
+    virtual list<list<Vertex>> sort2DList(list<list<Vertex>> l);
+    virtual list<Vertex> sortList(list<Vertex> l, int mode=0);
 public:
 	Shape(Vertex* vArr, int len, int innerLen) {
 		for (int i = 0, n = 0; i < len; i++) {
 			list<Vertex> sidesTemp;
+            float sumX = 0, sumY = 0, sumZ = 0;
 			for (int j = 0; j < innerLen; j++, n++) {
-				sidesTemp.push_back(*(vArr + n));
+                Vertex current = *(vArr + n);
+                sidesTemp.push_back(current);
+                sumX += current.getX();
+                sumY += current.getY();
+                sumZ += current.getZ();
 			}
 			sides.push_back(sidesTemp);
+            sideCentres.push_back(Vertex(sumX / 3, sumY / 3, sumZ / 3, 0, 0, 0));
 		}
 	}
 	virtual void draw();
